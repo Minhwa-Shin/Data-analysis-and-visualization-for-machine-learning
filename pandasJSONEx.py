85d9ac0a261101fa74eef27244e73d0cd399695c
Minhwa-Shin <65149183+Minhwa-Shin@users.noreply.github.com>
2021-04-18 23:30:44 +0900

    Create pandasJSONEx.py
    pandas json file example

---
+++ pandasJSONEx.py
@@ -0,0 +1,16 @@
+import json
+import pandas as pd
+
+obj = """
+{"name":"Wes",
+ "places_lived":["United States","Spain","Germany"],
+ "pet":null,
+ "siblings":[{"name":"Scott","age":25,"pet":"Zuko"},
+             {"name":"Katie","age":33,"pet":"Cisco"}]
+}
+"""
+result=json.loads(obj)
+print(result,'\n')
+
+siblings=pd.DataFrame(result['siblings'],index=['name','age'])
+print(siblings,'\n')
