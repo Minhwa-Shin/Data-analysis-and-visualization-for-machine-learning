c09c15af1dda26de3e017e2eb2d090545c68421a
Minhwa-Shin <65149183+Minhwa-Shin@users.noreply.github.com>
2021-04-20 14:29:13 +0900

    Create pandasPlotEx.py
    pandas plot example

---
+++ pandasPlotEx.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig,axes=plt.subplots(nrows=1,ncols=2)
+ax1,ax2=axes.ravel()
+data_frame=pd.DataFrame(np.random.rand(5,3),
+                        index=['customer1','customer2','customer3','customer4','customer5'],
+                        columns=pd.Index(['Metric 1','Metric 2','Metric 3'],name='Metrics'))
+
+print(data_frame,'\n')
+
+data_frame.plot(kind='bar',ax=ax1,alpha=0.75,title='Bar Plot')
+plt.setp(ax1.get_xticklabels(),rotation=45,fontsize=10)
+plt.setp(ax1.get_yticklabels(),rotation=0,fontsize=10)
+ax1.set_xlabel('Customer')
+ax1.set_ylabel('Value')
+ax1.xaxis.set_ticks_position('bottom')
+ax1.yaxis.set_ticks_position('left')
+
+colors=dict(boxes='DarkBlue',whiskers='Gray',medians='Red',caps='Black')
+data_frame.plot(kind='box',color=colors,sym='r.',ax=ax2,title='Box Plot')
+plt.setp(ax2.get_xticklabels(),rotation=45,fontsize=10)
+plt.setp(ax2.get_yticklabels(),rotation=0,fontsize=10)
+ax2.set_xlabel('Metric')
+ax2.set_ylabel('Value')
+ax2.xaxis.set_ticks_position('bottom')
+ax2.yaxis.set_ticks_position('left')
+
+plt.savefig('pandas_plots.png',dpi=400,bbox_inches='tight')
+plt.show()
